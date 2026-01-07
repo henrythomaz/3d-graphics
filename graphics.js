de69a13addd02a2eaee995369fde4e5f9e6c3ca7
henrythomaz <henrythcampos@gmail.com>
2026-01-07 10:12:58 -0300

    feat: Add graphics.js
    I tried to make my HTML dynamic, where there are buttons that are generated automatically.

---
+++ graphics.js
@@ -0,0 +1,42 @@
+// Não funciona no github Pages
+
+// import fs from "fs";
+
+// const caminhoPasta = __dirname + "/graphics";
+
+// try {
+//   const graficos = fs.readdirSync(caminhoPasta);
+
+//   console.log("Arquivos encontrados: ");
+//   graficos.forEach((nomeGrafico) => {
+//     console.log(nomeGrafico);
+//   })
+// } catch(err) {
+//   console.log("Erro ao ler os gráficos", err);
+// }
+
+// Funciona no Github Pages
+
+const USER = "henrythomaz";
+const REPO = "3d-graphics";
+const FOLDER = "graphics";
+
+async function listarArquivosGraficosGitHub() {
+  const url = `https://github.com/repos/${USER}/${REPO}/contents/${FOLDER}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log("Arquivos de graficos no GitHub");
+    data.forEach((file) => {
+      if(file.type == "file") {
+        console.log(file.name);
+      }
+    })
+    
+  } catch (err) {
+    console.log("Erro ao acessar a API do GitHb. :(", err);
+  }
+}
+listarArquivosGraficosGitHub();
